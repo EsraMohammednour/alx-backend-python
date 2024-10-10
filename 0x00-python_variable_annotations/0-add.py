c44f5dfd9dbabe09873785e1ae78ee80549e2e15
esraa <esraa2023112@gmail>
2024-10-10 21:56:37 +0300

    done

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """module for write type annotated function"""
 def add(a: float, b: float) -> float:
     '''fuction that add two float number
     Args:
-        a (float): float number
-        b (float): float number
+        a: float number
+        b: float number
     Returns:
-        float: number
+        float number
     '''
     return a + b
