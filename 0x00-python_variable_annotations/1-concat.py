b1c8f9db8fb15a3ed816b28520292a549f722c91
esraa <esraa2023112@gmail>
2024-10-10 22:11:51 +0300

    done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module that write type annotated
+"""
+def concat(str1: str, str2: str) -> str:
+    '''function that concatinate two string
+    Args:
+        str1: string value
+        str2:string  value
+    Returns:
+        string arrary
+    '''
+    return str1 + str2
