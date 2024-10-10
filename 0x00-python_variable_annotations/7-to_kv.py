e261b444ccdf28ed73e7dcd2d9db8e81130d6a89
esraa <esraa2023112@gmail>
2024-10-10 23:52:39 +0300

    done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''modlue to type annotated'''
-from typing import Union, List, Tuple
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
