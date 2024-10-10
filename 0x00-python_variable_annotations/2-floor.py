b69a0ddd1e5aa9c4be0c393a8fd74a2e042ef9ca
esraa <esraa2023112@gmail>
2024-10-10 22:24:18 +0300

    done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''module that write type annotated floor function'''
+
+
+def floor(n: float) -> int:
+    '''function that calculate floor 
+    Args:
+        n: float number
+    Returns:
+        integer number
+    '''
+    return int(n)
