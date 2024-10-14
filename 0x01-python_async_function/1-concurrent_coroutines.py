a15ac9bc449662e619246a409e89f3d24ca0e974
esra <esraa2023112@gmail.com>
2024-10-14 20:26:10 +0300

    done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,9 +4,9 @@ module of the wait_n coroutine
 '''
 import asyncio
 from typing import List
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
+
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''function that calculate wait_n'''
     x = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
